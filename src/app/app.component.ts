d492b87ed4f66e43a6196737961f95bad45fce65
TxnUkeGrl <txnukegrl@gmail.com>
2021-08-12 14:34:31 -0400

    adding TS & HTML content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { NumberFormatStyle } from '@angular/common';
 import { Component } from '@angular/core';
+import { TODOinterface } from './todointerface';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularTODOapp';
+
+  todolist: TODOinterface[] = [
+    {
+    task:'complete Angular lab',
+    completed: false
+    },
+    {
+    task: 'complete resume',
+    completed: true
+    },
+  ];
+
+  getTODOlist(): TODOinterface[]
+  {
+    let result: TODOinterface[] = [];
+    this.todolist.forEach((item: TODOinterface) => {
+      result.push(item);
+    });    
+    return result;
+  }
+
+  completeTask(task: string): void
+  {//this refers to the class it is within
+    this.todolist.forEach(element => {
+      if(element.task == task){
+        element.completed = true;
+      }
+    });
+  }
+
+  addTask(task: string): void{
+    let newTask: TODOinterface = {
+      task: task,
+      completed: false
+    };
+
+    this.todolist.push(newTask);
+  }
 }
---
+++ src/app/todointerface.ts
@@ -0,0 +1,5 @@
+export interface TODOinterface 
+{
+    task: string;
+    completed: boolean;
+}
